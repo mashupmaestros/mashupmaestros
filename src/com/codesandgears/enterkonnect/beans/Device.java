a2125945f2ec8a082c93737e7ff29e3101764c1e
Mohit <mashupmaestros@gmail.com>
2015-03-21 00:47:14 +0530

    cdcfv

--- src/com/codesandgears/enterkonnect/beans/Device.java
+++ src/com/codesandgears/enterkonnect/beans/Device.java
@@ -1,35 +1,64 @@
 package com.codesandgears.enterkonnect.beans;
 
-import com.googlecode.objectify.annotation.Id;
-
+import com.google.appengine.api.datastore.Blob;
 import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
 
 @Entity
 public class Device {
 	
 	@Id
-    Long id;
-
-    private String macid;
-    private float position;
-    
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+    private String macid;	
+    private String position;
+    private String timeStamp;
+    @Index
+    private String firstname;
+    private Blob profilepicture;
+    private double xcord;
+    private double ycord;
+	
 	public String getMacid() {
 		return macid;
 	}
 	public void setMacid(String macid) {
 		this.macid = macid;
 	}
-	public float getPosition() {
+	public String getPosition() {
 		return position;
 	}
-	public void setPosition(float position) {
+	public void setPosition(String position) {
 		this.position = position;
+	}
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public Blob getProfilepicture() {
+		return profilepicture;
+	}
+	public void setProfilepicture(Blob profilepicture) {
+		this.profilepicture = profilepicture;
+	}
+	public double getXcord() {
+		return xcord;
+	}
+	public void setXcord(double xcord) {
+		this.xcord = xcord;
+	}
+	public double getYcord() {
+		return ycord;
+	}
+	public void setYcord(double ycord) {
+		this.ycord = ycord;
 	}
     
     
